113e90bb109c23703f2a7bb5ffdf10de51ec523e
codecrook <hello@codecrook.io>
2018-12-01 11:40:04 +0530

    Created Editor Activity and Added code to add a book from it

--- app/src/main/java/io/codecrook/bookinventory/EditorActivity.java
+++ app/src/main/java/io/codecrook/bookinventory/EditorActivity.java
@@ -7,8 +7,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import io.codecrook.bookinventory.data.BookContract;
+import io.codecrook.bookinventory.data.BookContract.BookEntry;
 import io.codecrook.bookinventory.data.BookDbHelper;
 
 public class EditorActivity extends AppCompatActivity {
@@ -26,32 +27,58 @@ public class EditorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
 
-        EditText productNameEditText = findViewById(R.id.product_name_edit_text);
-        EditText productPriceEditText = findViewById(R.id.product_price_edit_text);
-        EditText productQuantityEditText = findViewById(R.id.product_quantity_edit_text);
-        EditText supplierNameEditText = findViewById(R.id.supplier_name_edit_text);
-        EditText supplierPhoneNoEditText = findViewById(R.id.supplier_phone_edit_text);
+        productNameEditText = findViewById(R.id.product_name_edit_text);
+        productPriceEditText = findViewById(R.id.product_price_edit_text);
+        productQuantityEditText = findViewById(R.id.product_quantity_edit_text);
+        supplierNameEditText = findViewById(R.id.supplier_name_edit_text);
+        supplierPhoneNoEditText = findViewById(R.id.supplier_phone_edit_text);
+    }
+
+    private void insertBook() {
+
+        String productName = productNameEditText.getText().toString().trim();
+        String productPriceText = productPriceEditText.getText().toString();
+        int productPrice = Integer.parseInt(productPriceText);
+        String productQuantityText = productQuantityEditText.getText().toString();
+        int productQuantity = Integer.parseInt(productQuantityText);
+        String supplierName = supplierNameEditText.getText().toString().trim();
+        String supplierPhone = supplierPhoneNoEditText.getText().toString().trim();
+
+
+        BookDbHelper mDbHelper = new BookDbHelper(this);
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(BookEntry.COLUMN_PRODUCT_NAME, productName);
+        values.put(BookEntry.COLUMN_PRODUCT_PRICE, productPrice);
+        values.put(BookEntry.COLUMN_PRODUCT_QUANTITY, productQuantity);
+        values.put(BookEntry.COLUMN_PRODUCT_SUPPLIER_NAME, supplierName);
+        values.put(BookEntry.COLUMN_PRODUCT_SUPPLIER_PHONE_NO, supplierPhone);
+
+        long newRowId = db.insert(BookEntry.TABLE_NAME, null, values);
+
+        if (newRowId == -1) {
+            Toast.makeText(this, "Error With Saving book", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Book saved with ID" +newRowId, Toast.LENGTH_SHORT).show();
+        }
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu options from the res/menu/menu_editor.xml file.
-        // This adds menu items to the app bar.
         getMenuInflater().inflate(R.menu.menu_editor, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // User clicked on a menu option in the app bar overflow menu
         switch (item.getItemId()) {
-            // Respond to a click on the "Save" menu option
             case R.id.action_save:
+                insertBook();
+                finish();
                 return true;
-            // Respond to a click on the "Delete" menu option
             case R.id.action_delete:
-                // Do nothing for now
                 return true;
         }
         return super.onOptionsItemSelected(item);
